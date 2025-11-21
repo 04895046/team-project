1118b6af5b489474656958e94256ecc41a6484f0
i-2049 <evaisabelmelo@gmail.com>
2025-11-21 14:01:48 -0500

    Add item entity

---
+++ src/main/java/Battle_System/Entity/User.java
@@ -0,0 +1,177 @@
+package Battle_System.Entity;
+
+import Battle_System.GameAPI.SrdMonsterDetail;
+import Battle_System.Entity.Inventory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class User {
+    //1. As a user I want to have an initial ATK and HP value when the game starts.
+    //2. As a user I want to be able to initiate a battle so that I can attack my opponent/monster or choose flee to aviod the fight.
+    //3. As a user I want to be able to access my inventory so that I can equip or use items.
+    public String NAME;
+    private double HP;
+    private double bonusHP = 0;
+    private double DMG = 8;
+    private double DEF = 0;
+    private Inventory inventory;
+
+    /**
+     * The constructor for User class.
+     */
+    public User() {
+        Random random = new Random();
+        HP = random.nextInt(11) + 20;
+        inventory = new Inventory(new ArrayList<>());
+    }
+
+    // TODO: finish the override
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    /**
+     * The getter for HP.
+     */
+    public double getHP() {
+        return HP;
+    }
+
+    /**
+     * The getter for DMG.
+     */
+    public double getDMG() {
+        return DMG;
+    }
+
+    /**
+     * The getter for bonusHP.
+     */
+    public double getBonusHP(){return bonusHP;}
+
+    /**
+     * The getter for DEF.
+     */
+    public double getDEF() {return DEF;}
+
+    /**
+     * Decrease the bonus HP, the bonus HP is considered as shield. Return the value of the left damage to the HP.
+     */
+    public double bonusHPDecrease(double dmg){
+        if(bonusHP > dmg){
+            bonusHP -= dmg;
+            return 0;
+        }else {
+            double returnVal = dmg - bonusHP;
+            bonusHP = 0;
+            return returnVal;
+        }
+    }
+
+    /**
+     * Decrease the HP by the damage. The actual damage depends on the DEF of the user. Some of the damage will only
+     * affect the bonus HP, if the damage is high, then it will decrease the HP.
+     */
+    public void HPDecrease(double dmg){
+        double damage = dmg * (1 - 0.08 * getDEF());
+        double leftDamage = bonusHPDecrease(damage);
+        HP -= leftDamage;
+        if (HP < 0) HP = 0;
+    }
+
+    /**
+     * Add Bonus HP
+     */
+    public void addBonusHP(double hp){
+        bonusHP += hp;
+    }
+
+    /**
+     * Add DMG, this method will be called when user equip the weapon or change the weapon.
+     */
+    public void addDMG(double dmg){DMG += dmg;}
+
+    /**
+     * Decrease DMG, this method will be called when user unequip the weapon or change the weapon.
+     */
+    public void decreaseDMG(double dmg){DMG -= dmg;}
+
+    /**
+     * Add DEF, this method will be called when user equip the armour or change the armour.
+     */
+    public void addDEF(double def){DEF += def;}
+
+    /**
+     * Decrease DEF, this method will be called when user unequip the armour or change the armour.
+     */
+    public void decreaseDEF(double def){DEF -= def;}
+
+    /**
+     * add item to user's inventory
+     * @param item
+     */
+    public void addItem(Item item){
+        inventory.addItem(item);
+    }
+
+    /**
+     * remove item from inventory
+     * @param item
+     */
+    public void removeItem(Item item){
+        inventory.removeItem(item);
+    }
+
+    /**
+     *
+     * @param name of item being searched for
+     * @return item as an item
+     */
+    public Item getItemByName(String name) {
+        return inventory.getItemByName(name);
+    }
+
+    /**
+     *
+     * @return inventory of the user
+     */
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    /**
+     *
+     * @return boolean whether the inventory contains any items
+     */
+    public boolean hasItems() {
+    return !inventory.getItems().isEmpty();
+    }
+
+    /**
+     * Return true if the HP of the user is greater than 0, which indicate the user is still alive.
+     */
+    public boolean isAlive() {return HP > 0;}
+
+    /**
+     * Ask the user to answer a quiz if they answered correctly then the user successfully attack.
+     */
+    public Boolean successAttack(){
+        return true;
+        // TODO : answer the quiz correctly then return true in this method
+    }
+
+    /**
+     * If the user successfully attacked, then it returns the current DMG of the user. If the user didn't answer the
+     * quiz correctly then the returned DMG will be 0.
+     */
+    public double attack(){
+        if(successAttack()){
+            return getDMG();
+        }
+        else{
+            return 0;
+        }
+    }
+}
